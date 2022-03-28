575f8ede8d6a37f9932632d404f4a2a5ccbca130
Greg Schneider <greg53x@msn.com>
2022-03-27 18:11:37 -0600

    allow custom grid creation 1-100

--- script.js
+++ script.js
@@ -2,19 +2,22 @@
 // create 16 divs within each column div
 // append each column div to flex-grid div 
 
-function createColumns(){
+function createColumns(numColumns){
+    if ((numColumns > 100)||(numColumns<1)) {
+        return alert("Please select a number between 1-100");
+    }
     let column = document.createElement('div');
     let grid = document.querySelector('.flex-grid');
     let row = document.createElement('div');
     row.setAttribute('class','row');
     column.setAttribute('class','col');
     column.appendChild(row);
-    for (let i =0; i<15; i++){
+    for (let i =0; i<(numColumns-1); i++){
         column.appendChild(row.cloneNode(true));
     }
     grid.appendChild(column);
     
-    for (let j = 0; j<15; j++){
+    for (let j = 0; j<(numColumns-1); j++){
         
         grid.appendChild((document.querySelector('.col')).cloneNode(true));
     }
@@ -23,11 +26,11 @@ function createColumns(){
 function hoverButtonCreation() {
     let squares = document.querySelectorAll('.row');
     squares.forEach((row) => {
-        row.addEventListener('mouseover', () => {
+        row.addEventListener('mouseenter', () => {
             // row.setAttribute('style', 'background-color: red')
             row.classList.add('mouse-enter');
         });
     });
 }
-createColumns();
+createColumns(16);
 hoverButtonCreation();
